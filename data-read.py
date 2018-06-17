0f0c623dd2179864ac37d660ba8782434d982aec
Algirdas Č <algirdas.cic@gmail.com>
2018-06-17 21:12:19 +0300

    Initial commit

---
+++ data-read.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3.5
+from mitemp_bt.mitemp_bt_poller import MiTempBtPoller
+from mitemp_bt.mitemp_bt_poller import MI_TEMPERATURE, MI_HUMIDITY, MI_BATTERY
+from btlewrap.bluepy import BluepyBackend
+import paho.mqtt.client as mqtt
+import configparser
+import os
+import json
+import datetime
+
+workdir = os.path.dirname(os.path.realpath(__file__))
+config = configparser.ConfigParser()
+config.read("{0}/devices.ini".format(workdir))
+
+devices = config.sections()
+
+# Init MQTT
+mqtt_config = configparser.ConfigParser()
+mqtt_config.read("{0}/mqtt.ini".format(workdir))
+mqtt_broker = mqtt_config["broker"]
+
+try:
+    mqtt_client = mqtt.Client("RPI2")
+    mqtt_client.connect(host=mqtt_broker.get("host"), port=mqtt_broker.getint("port"))
+except Exception as ex:
+    print("Cannot connect to MQTT: {0}".format(str(ex)))
+    exit(1)
+
+for device in devices:
+    mac = config[device].get("device_mac")
+    poller = MiTempBtPoller(mac, BluepyBackend)
+    try:
+        temperature = poller.parameter_value(MI_TEMPERATURE)
+        humidity = poller.parameter_value(MI_HUMIDITY)
+        battery = poller.parameter_value(MI_BATTERY)
+        json = json.dumps({"temperature": temperature, "humidity": humidity, "battery": battery})
+        print(datetime.datetime.now(), device, " : ", json)
+        mqtt_client.publish(config[device].get("topic"), json, retain=True)
+    except Exception as e:
+        print("Error polling device {0}:".format(device))
+        print(str(e))
