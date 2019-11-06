5b8500942a609aca10776392de519abcb18a5975
Algirdas Č <algirdas.cic@gmail.com>
2019-11-06 10:02:49 +0200

    Solved MQTT timeout while long polling BTLE device

--- data-read.py
+++ data-read.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+
 from mitemp.mitemp_bt.mitemp_bt_poller import MiTempBtPoller
 from mitemp.mitemp_bt.mitemp_bt_poller import MI_TEMPERATURE, MI_HUMIDITY, MI_BATTERY
 from btlewrap.bluepy import BluepyBackend
 from bluepy.btle import BTLEException
-import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
 import traceback
 import configparser
 import os
@@ -16,28 +17,11 @@ config.read("{0}/devices.ini".format(workdir))
 
 devices = config.sections()
 
-# Init MQTT
-mqtt_config = configparser.ConfigParser()
-mqtt_config.read("{0}/mqtt.ini".format(workdir))
-mqtt_broker_cfg = mqtt_config["broker"]
-
-try:
-    mqtt_client = mqtt.Client(mqtt_broker_cfg.get("client"))
-
-    mqtt_username = mqtt_broker_cfg.get("username")
-    mqtt_password = mqtt_broker_cfg.get("password")
-
-    if mqtt_username:
-        mqtt_client.username_pw_set(mqtt_username, mqtt_password)
-
-    mqtt_client.connect(host=mqtt_broker_cfg.get("host"), port=mqtt_broker_cfg.getint("port"))
-except Exception as ex:
-    print("Cannot connect to MQTT: {0}".format(str(ex)))
-    exit(1)
-
 # Averages
 averages = configparser.ConfigParser()
 averages.read("{0}/averages.ini".format(workdir))
+
+messages = []
 
 for device in devices:
 
@@ -97,18 +81,35 @@ for device in devices:
             })
 
         print(datetime.datetime.now(), device, " : ", data)
-
-        mqtt_client.publish(config[device].get("topic"), data, retain=config[device].getboolean("retain", False))
-        mqtt_client.publish(config[device].get("availability_topic"), "online")
+        messages.append({'topic': config[device].get("topic"), 'payload': data, 'retain': config[device].getboolean("retain", False)})
+        availability = 'online'
     except BTLEException as e:
-        mqtt_client.publish(config[device].get("availability_topic"), "offline")
-        print("Error connecting to device {0}: {1}".format(device, str(e)))
+        availability = 'offline'
+        print(datetime.datetime.now(), "Error connecting to device {0}: {1}".format(device, str(e)))
     except Exception as e:
-        mqtt_client.publish(config[device].get("availability_topic"), "offline")
-        print("Error polling device {0}. Device might be unreachable or offline.".format(device))
+        availability = 'offline'
+        print(datetime.datetime.now(), "Error polling device {0}. Device might be unreachable or offline.".format(device))
         # print(traceback.print_exc())
+    finally:
+        messages.append({'topic': config[device].get("availability_topic"), 'payload': availability, 'retain': config[device].getboolean("retain", False)})
+
+
+# Init MQTT
+mqtt_config = configparser.ConfigParser()
+mqtt_config.read("{0}/mqtt.ini".format(workdir))
+mqtt_broker_cfg = mqtt_config["broker"]
+
+try:
+    auth = None
+    mqtt_username = mqtt_broker_cfg.get("username")
+    mqtt_password = mqtt_broker_cfg.get("password")
+
+    if mqtt_username:
+        auth = {"username": mqtt_username, "password": mqtt_password}
+
+    publish.multiple(messages, hostname=mqtt_broker_cfg.get("host"), port=mqtt_broker_cfg.getint("port"), client_id=mqtt_broker_cfg.get("client"), auth=auth)
+except Exception as ex:
+    print(datetime.datetime.now(), "Error publishing to MQTT: {0}".format(str(ex)))
 
 with open("{0}/averages.ini".format(workdir), "w") as averages_file:
     averages.write(averages_file)
-
-mqtt_client.disconnect()
